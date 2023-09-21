ce8da6247c2986c0f6856fbea3568ea0699bc591
Michael Gulotta <gulottam@gmail.com>
2023-09-21 11:29:23 -0400

    GET actions complete

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,23 @@
 // add middlewares here related to actions
+const Actions = require('./actions-model')
+
+async function validateId(req, res, next) {
+    try {
+        const action = await Actions.get(req.params.id)
+        if(!action) {
+            next({status: 404, message: 'id not found'})
+        } else {
+            req.action = action
+            next()
+        }
+    } catch(err) {
+        req.status(500).json({
+            message: 'problem finding id'
+        })
+    }
+    
+}
+
+module.exports = {
+    validateId
+}
--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,39 @@
 // Write your "actions" router here!
+const express = require('express')
+
+const Actions = require('./actions-model')
+
+const {
+validateId
+} = require('./actions-middlware')
+
+const router = express.Router()
+
+router.get('/', (req, res, next) => {
+    Actions.get()
+    .then(actions => {
+        res.status(200).json(actions)
+    })
+    .catch(next)
+
+})
+
+router.get('/:id', validateId, (req, res) => {
+    res.json(req.action)
+})
+
+router.post('/:id', (req, res) => {
+
+})
+
+router.put('/:id', (req, res) => {
+
+})
+
+router.delete('/:id', (req, res) => {
+
+})
+
+
+
+module.exports = router
